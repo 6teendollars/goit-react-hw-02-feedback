0342519814712c13f4b1a37929f68ceecb73d329
6teendollars <91662992+6teendollars@users.noreply.github.com>
2023-05-13 07:56:23 +0200

    p

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,60 +1,70 @@
-import React, { Component } from 'react';
+// import React, { Component } from "react";
 // import css from './App.module.css';
-import { Section } from './Section/Section';
-import { FeedbackOptions } from './FreedbackOptions/FeedbackOption';
-import { Statistics } from './Statistics/Statistics';
-import { Notification } from './Notification/Notification';
+import { Section } from "./Section/Section";
+import { FeedbackOptions } from "./FreedbackOptions/FeedbackOption";
+import { Statistics } from "./Statistics/Statistics";
+import { Notification } from "./Notification/Notification";
+import { TEST } from "./TEST/TEST";
+import React, { useState } from "react";
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const countTotalFeedback = () => {
+    return good + neutral + bad;
   };
 
-  countTotalFeedback = () => {
-    return Object.values(this.state).reduce((acc, item) => acc + item, 0);
-  };
-
-  countPositiveFeedbackPercentage = () => {
-    const total = this.countTotalFeedback();
+  const countPositiveFeedbackPercentage = () => {
+    const total = countTotalFeedback();
     if (!total) {
       return 0;
     }
-    return Math.round((this.state.good / this.countTotalFeedback()) * 100);
-  };
-  onLeaveFeedback = evt => {
-    this.setState(prevState => ({
-      [evt.target.name]: prevState[evt.target.name] + 1,
-    }));
+    return Math.round((good / total) * 100);
   };
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    const options = Object.keys(this.state);
-    const total = this.countTotalFeedback();
-    return (
-      <div>
-        <Section title="Please, leave feedback">
-          <FeedbackOptions
-            options={options}
-            onLeaveFeedback={this.onLeaveFeedback}
+  const onLeaveFeedback = (evt) => {
+    const feedbackType = evt.target.name;
+
+    switch (feedbackType) {
+      case "good":
+        setGood((prevGood) => prevGood + 1);
+        break;
+      case "neutral":
+        setNeutral((prevNeutral) => prevNeutral + 1);
+        break;
+      case "bad":
+        setBad((prevBad) => prevBad + 1);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const options = ["good", "neutral", "bad"];
+
+  return (
+    <div>
+      <Section title="Please, leave feedback">
+        <FeedbackOptions options={options} onLeaveFeedback={onLeaveFeedback} />
+      </Section>
+      <Section title="Statistics">
+        {countTotalFeedback() ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={countTotalFeedback()}
+            positivePercentage={countPositiveFeedbackPercentage()}
           />
-        </Section>
-        <Section title="Statistics">
-          {total ? (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={this.countTotalFeedback()}
-              positivePercentage={this.countPositiveFeedbackPercentage()}
-            />
-          ) : (
-            <Notification message="No feedback given" />
-          )}
-        </Section>
-      </div>
-    );
-  }
-}
+        ) : (
+          <Notification message="No feedback given" />
+        )}
+      </Section>
+      <TEST />
+    </div>
+  );
+};
+
+// export default App;
---
+++ src/components/TEST/TEST.jsx
@@ -0,0 +1,15 @@
+import { useState, useEffect } from "react";
+
+export const TEST = () => {
+  const [value, setValue] = useState(0);
+  useEffect(() => {
+    document.title = `You clicked ${value} times`;
+  });
+
+  return (
+    <div>
+      <p>You clicked {value} times</p>
+      <button onClick={() => setValue(value + 1)}>Click me</button>
+    </div>
+  );
+};
